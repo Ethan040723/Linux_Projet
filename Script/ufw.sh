1beca7dc4fcfa38bce3930827d28af18bb7db305
MBAIHINGABE <ianick.affaire@gmail.com>
2025-11-21 18:25:22 +0100

    Add files via upload

---
+++ Script/ufw.sh
@@ -0,0 +1,13 @@
+
+
+    sudo apt update
+    sudo apt install -y ufw
+
+
+sudo ufw enable
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw reload
+sudo ufw status
